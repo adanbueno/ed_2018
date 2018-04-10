f15176dc29d332809d997d2316ee21729ca25f26
Adan Bueno <adan_bueno@hotmail.com>
2018-04-10 09:59:21 -0300

    Concluido

---
+++ 04_Fractais/README.md
@@ -0,0 +1,34 @@
+##04_Fractais
+
+#Objetivo:
+
+• O seu objetivo é aprender recursão para implementar o código que produzirá os fractais da pasta imagens. 
+Faça o máximo que puder. Se enganchar em algum, pesquise na internet.
+
+Como passado em sala o professor deu um conceito a mais sobre recursão
+e fez sua utilização para desenhar fractais, em sala foi mostrado o 
+desenho da arvore e do triangulo onde mostra o uso da chamada de função e
+recursão. Os outros 3 fractais eu fiz junto com meu amigo de EC Jeronimo, 
+na qual procuramos fazer os fractais da maneira mais simples e clara possivel
+no principio tentei usar formulas matematicas para poder desenhar porque
+tinha visto na internet a aplicação, mas seria bastante complicado, ai
+assisti umas duas vezes a video aula dos fractais e como era desenvolvido
+cada exemplo passo a passo, seguindo o raciocinio eu fui fazer primeiramente
+os dos circulos, na qual tive que usar um for pra poder fazer pra ir de 
+0 ate 6, para poder desenhar os circulos certos, ajustei o angulo para 60 graus, e fui chamando as funções ate a hora de chamar a função circulos e
+ter que a gambiarra pra dar certo.
+O segundo codigo foi o floco de neve, esse fiz junto com jeronimo que foi um pouco complicado de pegar a logica, na função criei um aux, e tive que 
+ajustar o angulo para 72, se o aux fosse 0 ou o lado menor 3 ele retornava
+se não ele começava a fazer os desenhos, foi bastante complicado essa parte
+tive que pedir ajuda a um amigo que ja fez essa diciplina de ed para nos
+ajudar nessa parte.
+Por ultimo o mais dificil e trabalhoso foi o do trigo, esse levei em media uns 2 a 3 dias para concluir, esse teve que mexer muito com seu angulo para
+poder fazer o desenho, tive que usar dois for para poder fazer ambos indo de
+0 ate 4, foi um metodo meio braçal tenho que ficar chamando a função e 
+multiplicando os lados e o angulo, mas também depois consegui fazer de um metodo mais pratico, utilizando só um for e apenas mudando o angulo e
+e dividino os lados por 4.
+No total demorei uma semana nesse codigo, precisando de ajuda em algumas
+partes, isso mostra que ainda estou muito fraco na linguagem e em aprender
+totalmente recursão.
+
+
--- 04_Fractais/main.cpp
+++ 04_Fractais/main.cpp
@@ -1,6 +1,5 @@
 #include <lib/pen.h>
 
-
 void triangulo(Pen &p, int lado){
     if(lado < 10)
         return;
@@ -10,14 +9,79 @@ void triangulo(Pen &p, int lado){
         triangulo(p, lado/2);
     }
 }
+void circulos(Pen& p, double lado){
+    if(lado < 1)
+        return;
+    p.circle(lado);
+    for(int i = 0; i < 6; i++){
+        p.left(60);
+        p.up();
+        p.walk(lado);
+        p.down();
+        circulos(p,lado*2/6);
+        p.up();
+        p.walk(-lado);
+        p.down();
+    }
+}
+
+void trigo(Pen& p, int lado){
+    float angulo = 45;
+
+    if(lado <= 0)
+        return;
+    for(int i = 0; i < 4; i++){
+        p.walk(lado);
+        p.left(angulo);
+        trigo(p, lado * 0.30);
+        p.right(angulo*2);
+        trigo(p, lado * 0.30);
+        p.left(angulo);
+    }
+    for(int i = 0; i < 4; i++){
+        p.walk(-lado);
+    }
+}
+
+void trigo2(Pen& p, double lado){
+    if (lado < 0.1)
+        return;
+    p.setColor(255, 255, 255);
+
+    for(int i = 0; i < 4; i++){
+        p.walk(lado);
+        p.right(45);
+        trigo2(p,lado/4);
+        p.left(90);
+        trigo2(p,lado/4);
+        p.right(45);
+    }
+   p.setColor(rand() % 255,rand() % 255,rand() % 255);
+   p.walk(-lado*4);
+}
+void frozen(Pen& p,int aux, int lado){
+    float angulo = 72;
+    if(aux == 0 || lado < 3)
+    return;
+
+    p.walk(lado);
+    gelo(p,5, lado *0.36);
+    p.walk(-lado);
+    p.right(angulo); // 360*0.2
+    p.setColor(rand() % 255,rand() % 255,rand() % 255);
+    aux--;
+    gelo(p,aux, lado);
+}
+
+
 
 void arvore(Pen& p, int lado){
     float ang = 35;
-    float dec = 14;
-    lado -= (p.rand() % 7 - 2);
+    float dec = 0.75;
+    lado -= (p.rand() % 7 - 3);
     if(lado < 10)
         return;
-    p.setColor(rand() % 78, rand() % 56, rand() % 77);
+    p.setColor(rand() % 255, rand() % 255, rand() % 255);
     p.walk(lado);
     p.right(ang);
     arvore(p, lado * dec);
@@ -25,11 +89,8 @@ void arvore(Pen& p, int lado){
     arvore(p, lado * dec);
     p.left(ang);
     arvore(p, lado * dec);
-    p.left(ang);
-    arvore(p, lado * dec);
-    p.left(ang);
-    arvore(p, lado * dec);
     p.right(ang);
+    //p.setColor(sf::Color::Red);
     p.walk(-lado);
 }
 
@@ -37,15 +98,22 @@ void fractal(){
     Pen p(800, 600);
     p.setThickness(2);
     //coloca o pincel na posicao x 300 y 500
-    p.setXY(400, 500);
+    p.setXY(400, 300);// floco de neve
+    //p.setXY(400, 500);// trigo
 
     //faz o pincel apontar pra direita
-    p.setHeading(90);
+    //p.setHeading(0);//para o circulo
+    p.setHeading(90);//para o trigo
+    //p.setHeading(60);//para floco de neve
     //se speed = 0 entao ele faz o mais rapido possivel
     //se foi a partir de 1 ele controla a velocidade
     p.setSpeed(0);
 
-    arvore(p, 100);
+    //circulos(p,200);
+    //trigo2(p,80);
+    //trigo(p,80);
+    //frozen(p,5,200);
+    //arvore(p, 100);
     //triangulo(p, 400);
     //espera clicar no botao de fechar
     p.wait();
